6629f7e6ddc339ab4f63629a410645c40a3fe4c3
Venkatagiriraju U <venkatagiriraju045@gmail.com>
2024-03-12 11:40:06 +0530

    first commit

--- frontend/src/components/OutPatientBill.js
+++ frontend/src/components/OutPatientBill.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './CSS/InPatientBill.css';
 
 const OutPatientBill = () => {
     const [loading, setLoading] = useState(false);
--- frontend/src/components/PatientDetails.js
+++ frontend/src/components/PatientDetails.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './CSS/NewRecord.css';
 import OverlayRecord from './OverlayRecord';
 
 const PatientDetails = () => {

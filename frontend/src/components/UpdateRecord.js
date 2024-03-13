01b5e3977132ea95909cdcd8fe22c8e759dbe6ca
Venkatagiriraju U <venkatagiriraju045@gmail.com>
2024-03-13 08:34:31 +0530

    first commit

--- frontend/src/components/UpdateRecord.js
+++ frontend/src/components/UpdateRecord.js
@@ -2644,7 +2644,7 @@ const UpdateRecord = () => {
             const foundPatientBasicRecord = response.data;
             setPatient(foundPatientBasicRecord);
             setFounded(true);
-            setFirstRow(foundPatientBasicRecord.planTreatment[0].patientType === "");
+            // setFirstRow(foundPatientBasicRecord.planTreatment[0].patientType === "");
             console.log("patient ::::", foundPatientBasicRecord);
             // Introduce a delay of 500 milliseconds (adjust as needed)
             await delay(500);
